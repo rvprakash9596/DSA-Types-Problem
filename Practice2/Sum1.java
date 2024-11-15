4bac819bee6f02ff41c61c368752eca5811322c6
rvprakash9596 <raviprakash9596@gmail.com>
2024-11-15 07:47:30 +0530

    Addition of 2 numbers

---
+++ Practice2/Sum1.java
@@ -0,0 +1,7 @@
+//Sum of two numbers
+public class Sum1{
+    public static void main(String args[]){
+        int sum = 10+20;
+        System.out.println("Sum ="+sum);
+    }
+}
---
+++ Practice2/Sum2.java
@@ -0,0 +1,17 @@
+// Addtion of two numbers using user input
+import java.util.Scanner;
+public class Sum2{
+    public static void main(String args[]){
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter 1st Number :");
+        int fn = inp.nextInt();
+
+        System.out.print("Enter 2nd Number : ");
+        int sn  = inp.nextInt();
+
+        int sum = fn+sn;
+
+        System.out.println("The sum of "+fn+"+"+sn+" ="+sum);
+    }
+}
