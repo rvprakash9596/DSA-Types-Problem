d04556a6455b94d8e1a20306617040cfc96898bb
rvprakash9596 <raviprakash9596@gmail.com>
2024-11-15 07:47:58 +0530

    ..

--- Hello1.java
+++
@@ -1,8 +0,0 @@
-public class Hello1{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello ! Mr.");
-    }
-}
-
-//https://www.w3resource.com/java-exercises/basic/index.php
