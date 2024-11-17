717cb2d5dcec79ef081128d3f358194c00e427fe
rvprakash9596 <raviprakash9596@gmail.com>
2024-11-17 09:15:30 +0530

    Simple Pattern

---
+++ Practice8/Pattern1.java
@@ -0,0 +1,18 @@
+/*Write a Java program to display the following pattern.
+
+Sample Pattern :
+
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+ */
+
+public class Pattern1 {
+    public static void main(String args[]){
+        System.out.println("    J       A       V      V       A");
+        System.out.println("    J     A   A      V    V       A  A");
+        System.out.println("J   J    AAAAAAA       V V       AAAAAAA");
+        System.out.println(" JJJJ   A       A       V       A       A");
+    }
+}
