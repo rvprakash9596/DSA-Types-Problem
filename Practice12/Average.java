893876bb6b4058ff74fb85f9d0c8c5112c593315
rvprakash9596 <raviprakash9596@gmail.com>
2024-11-17 09:27:36 +0530

    Average of 5 Numbers

---
+++ Practice12/Average.java
@@ -0,0 +1,28 @@
+/*
+ Write a Java program that takes five numbers as input to calculate and print the average of the numbers.
+
+Test Data:
+Input first number: 10
+Input second number: 20
+Input third number: 30
+Input fourth number: 40
+Enter fifth number: 50
+ */
+import java.util.Scanner;
+public class Average{
+    public static void main(String args[]){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter 1st Number : ");
+        int a=inp.nextInt();
+        System.out.print("Enter 2nd Number : ");
+        int b=inp.nextInt();
+        System.out.print("Enter 3rd Number : ");
+        int c=inp.nextInt();
+        System.out.print("Enter 4th Number : ");
+        int d=inp.nextInt();
+        System.out.print("Enter 5th Number : ");
+        int e=inp.nextInt();
+        int avg=(a+b+c+d+e)/5;
+        System.out.print("The Average is : "+avg);
+    }
+}
