5d47b44a72c5e4c4bc06405bf153c2c46feb11ba
rvprakash9596 <raviprakash9596@gmail.com>
2024-11-22 20:33:24 +0530

    Sum of Digits using while()

--- Practice33/SumOfDigit.java
+++ Practice33/SumOfDigit.java
@@ -2,5 +2,16 @@ import java.util.Scanner;
 public class SumOfDigit{
     public static void main(String[] args) {
         Scanner inp=new Scanner(System.in);
+        System.out.print("Enter Number:");
+        long num =inp.nextLong();
+        System.out.printf("Sum of %d = %d", num, sumDigits(num));
+    }
+    public static int sumDigits(long num){
+        int sum=0;
+        while(num != 0){
+            sum+=num % 10;
+            num=num/10;
+        }
+        return sum;
     }
 }
