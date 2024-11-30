3ebf863886667488b9a59bd552caadb91a4df6d4
rvprakash9596 <raviprakash9596@gmail.com>
2024-11-30 19:51:41 +0530

    Star Pattern

--- Loops/For1.java
+++ Loops/For1.java
@@ -2,6 +2,7 @@ public class For1{
     public static void main(String[] args) {
         for(int i=1; i<=5; i++){
             System.out.println(i);
+            
         }
     }
 }
---
+++ Patterns/Star1.java
@@ -0,0 +1,8 @@
+public class Star1{
+    public static void main(String[] args){
+        int i;
+        for(i=1; i<=5; i++){
+            System.out.println("*");
+        }
+    }
+}
