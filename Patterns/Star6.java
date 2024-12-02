044090aa0775e8deb77f732044029e1dfeed31fa
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-02 22:20:54 +0530

    Patterns

--- Patterns/Star6.java
+++ Patterns/Star6.java
@@ -2,7 +2,24 @@ public class Star6 {
     public static void main(String[] args) {
         for(int i=1; i<=5; i++)
         {
-            
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=5; k>=i; k--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
+
+/*
+
+ *****
+  ****
+   ***
+    **
+     *
+*/
