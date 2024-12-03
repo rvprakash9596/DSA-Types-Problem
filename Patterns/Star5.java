97d12e5964c1aba1de382dd0120f71be56bdac76
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-03 22:22:45 +0530

    Star5

--- Patterns/Star5.java
+++ Patterns/Star5.java
@@ -2,7 +2,8 @@ public class Star5 {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            for (int j = 5; j >= i; j--) {
+            for (int j = 5; j >= i; j--) 
+            {
                 System.out.print(" ");
             }
             for (int k = 1; k <= i; k++) {
