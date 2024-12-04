fbb46143d27c89be49580641b4deb60a4f342200
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-04 08:50:20 +0530

    DuplicateElemen

---
+++ DuplicateElement/DuplicateEle.java
@@ -0,0 +1,10 @@
+package DuplicateElement;
+
+public class DuplicateEle {
+    public static void main(String[] args) {
+        String arr[]={"Java","C","C++","Python","Java"};
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
