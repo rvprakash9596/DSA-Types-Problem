a7139b1f78ef541b3d60a6af430b370a98141108
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-04 09:06:27 +0530

    DuplicateElemen

--- DuplicateElement/DuplicateEle.java
+++ DuplicateElement/DuplicateEle.java
@@ -2,8 +2,11 @@ package DuplicateElement;
 
 public class DuplicateEle {
     public static void main(String[] args) {
-        String arr[]={"Java","C","C++","Python","Java"};
+        String arr[]={"Java","C","C++","Python","Java","Python"};
+
         //Approach1
+
+        /*boolean flag=false;
         for(int i=0; i<arr.length; i++)
         {
             for(int j=i+1; j<arr.length; j++)
@@ -11,9 +14,18 @@ public class DuplicateEle {
                 if(arr[i]==arr[j])
                 {
                     System.out.println("Found Duplicate Element :"+arr[i]);
+                    flag=true;
                 }
             }
         }
-        
+        if(flag==false)
+        {
+            System.out.println("Duplicate Element Not Found");
+        }*/
+
+        //Approach 2 using Hash Method in Data Structure
+
+
+
     }
 }
