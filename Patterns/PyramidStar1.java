f75ef301019a203b324ed6b2c62a36bd99d99aef
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-04 22:55:39 +0530

    PyramidStar1

---
+++ Patterns/PyramidStar1.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+public class PyramidStar1{
+    public static void main(String[] args) {
+        int i,j,k,l;
+        for(i=1; i<=5; i++)
+        {
+            for(j=5; j>=i; j--)
+            {
+                System.out.print(" ");
+            }
+            for(k=1; k<=i; k++)
+            {
+                System.out.print("*");
+            }
+            for(l=2; l<=i; l++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
