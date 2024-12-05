66ba0a76656bd8ee1b667600fde862d37cb2601f
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-05 23:17:37 +0530

    Damru Shape Pattern

--- Patterns/Damru.java
+++ Patterns/Damru.java
@@ -1,6 +1,68 @@
 package Patterns;
+import java.util.Scanner;
 public class Damru {
     public static void main(String[] args) {
-        
+        int i,j,k,n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of Rows : ");
+        n=sc.nextInt();
+        for(i=0; i<=n-1; i++)
+        {
+            for(j=0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=i; k<=n-1; k++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println("");
+        }
+
+
+        for(i=n-1; i>=0; i--)
+        {
+            for(j=0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=i; k<=n-1; k++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println("");
+        }
+        sc.close();
+
     }    
 }
+
+
+
+/*
+
+Enter the number of Rows : 10
+
+* * * * * * * * * * 
+ * * * * * * * * * 
+  * * * * * * * * 
+   * * * * * * * 
+    * * * * * * 
+     * * * * * 
+      * * * * 
+       * * * 
+        * * 
+         *
+         *
+        * *
+       * * * 
+      * * * *
+     * * * * *
+    * * * * * *
+   * * * * * * *
+  * * * * * * * * 
+ * * * * * * * * *
+* * * * * * * * * *
+
+
+*/
