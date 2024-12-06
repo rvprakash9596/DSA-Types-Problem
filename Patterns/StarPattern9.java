a7c7326a6d2ff2393a21efc40c4bcabc18bd3289
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-06 20:15:42 +0530

    StarPattern9

---
+++ Patterns/StarPattern9.java
@@ -0,0 +1,32 @@
+package Patterns;
+public class StarPattern9{
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=5; k>=i; k--)
+            {
+                System.out.print("*");
+            }
+            for(int l=3; l>=i; l--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/*
+
+********
+  ******
+   ****
+    **
+     *
+
+*/
