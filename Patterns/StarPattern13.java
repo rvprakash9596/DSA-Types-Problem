4cbc8364bbf9223e242c872f07d060f5b17bdd47
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-08 22:28:51 +0530

    StarPattern13

--- Patterns/StarPattern13.java
+++ Patterns/StarPattern13.java
@@ -3,5 +3,18 @@ package Patterns;
 public class StarPattern13 {
     public static void main(String[] args) {
         int i,j,k;
+        for(i=1; i<=5; i++)
+        {
+            for(j=5; j>=i; j--)
+            {
+                System.out.print(" ");
+            }
+            for(k=1; k<(i*2); k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
