71233079868c046bb23bf4384cb717e08aad962f
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-08 22:44:47 +0530

    *     * *    *   *   *     *  *       *

--- Patterns/StarPattern14.java
+++ Patterns/StarPattern14.java
@@ -1,5 +1,38 @@
 package Patterns;
 
 public class StarPattern14 {
-    
+    public static void main(String[] args) {
+        int i,j,k;
+        for(i=1; i<=5; i++)
+        {
+            for(k=5; k>=i; k--)
+            {
+                System.out.print(" ");
+            }
+            for(j=1; j<(i*2); j++)
+            {
+                if(j>1 && j<(i*2)-1)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
 }
+
+/*
+
+     *
+    * *
+   *   *
+  *     *
+ *       *
+
+*/
