bca2914257e33599fa1af47aa933e8460737e686
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-18 12:46:26 +0530

    Printing odd numbers

---
+++ Practice48/OddNumber.java
@@ -0,0 +1,14 @@
+package Practice48;
+
+public class OddNumber {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++)
+        {
+            if(i%2 !=0)
+            {
+                System.out.print(i);
+            }
+            System.out.print("\t");
+        }
+    }
+}
