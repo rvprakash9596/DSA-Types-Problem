0b145cc256324360ab6ac38829d780806bd627e3
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-22 19:56:23 +0530

    Is element available in the array

---
+++ Arrays/ArrayContainsElement.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArrayContainsElement {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        
+        int[] array = new int[n];
+        
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        
+        System.out.print("Enter the element to search for: ");
+        int x = sc.nextInt();
+        
+        boolean found = false;
+
+        for (int element : array) {
+            if (element == x) {
+                found = true;
+                break;
+            }
+        }
+        
+        // Output the result
+        if (found) {
+            System.out.println("The array contains the element " + x + ".");
+        } else {
+            System.out.println("The array does not contain the element " + x + ".");
+        }
+        
+    }
+}
