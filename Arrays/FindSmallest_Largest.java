1a6e175b3180d55aeeda69dd12a9fe1c589624ad
rvprakash9596 <raviprakash9596@gmail.com>
2024-12-27 20:39:11 +0530

    Find Largest and Smallest Element in Array

---
+++ Arrays/FindSmallest_Largest.java
@@ -0,0 +1,42 @@
+/*
+package Arrays;
+public class FindSmallest_Largest{
+    public static void main(String[] args) {
+        
+    }
+}
+*/
+
+package Arrays;
+
+public class FindSmallest_Largest {
+    public static void main(String[] args) {
+        int[] numbers = {3, 5, 1, 8, -2, 7, 10};
+
+        int smallest = findSmallest(numbers);
+        int largest = findLargest(numbers);
+
+        System.out.println("Smallest element: " + smallest);
+        System.out.println("Largest element: " + largest);
+    }
+
+    public static int findSmallest(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static int findLargest(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
