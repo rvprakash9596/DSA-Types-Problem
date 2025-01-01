811efa23d31170d9e6b79358025d4ed886abd780
rvprakash9596 <raviprakash9596@gmail.com>
2025-01-01 22:55:44 +0530

    Split the main array into two sub array

---
+++ Arrays/SplitArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+import java.util.Arrays;
+
+public class SplitArray {
+    public static void main(String[] args) {
+        String[] names = {"Ravi", "Ritik", "Mukesh", "Shabbir", "Harsh","Akki","Tushar"};
+        int splitIndex = 3;// Split after the third element
+         // Create two sub-arrays
+         String[] firstHalf = Arrays.copyOfRange(names, 0, splitIndex);
+         String[] secondHalf = Arrays.copyOfRange(names, splitIndex, names.length);
+         System.out.println("First Half: " + Arrays.toString(firstHalf));
+         System.out.println("Second Half: " + Arrays.toString(secondHalf));
+    }
+}
